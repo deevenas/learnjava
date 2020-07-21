db76621cb2204c781829b7640419265f02cb58ff
deevena <sarkar.deevena@gmail.com>
2020-07-21 16:49:01 -0700

    initial commit

---
+++ src/learn/HomeWork.java
@@ -0,0 +1,32 @@
+public class HomeWork
+{
+    public void oregon()
+    {
+        System.out.print("This Summer I went to Oregon to see my cousins. While there, we:");
+    }
+    public void parade()
+    {
+        System.out.println("\n\n\nLed a flag brigade in a 4th of July Parade");
+    }
+    public void bbq()
+    {
+        System.out.println("\n\n\nHad a bbq with a whole bunch of cousins");
+    }
+    public void hiking()
+    {
+        System.out.println("\n\n\nWent hiking to see petroglyphs, and");
+    }
+    public void starfish()
+    {
+        System.out.println("\n\n\nFound a starfish at the beach");
+    }
+    public static void main(String[] army)
+    {
+        HomeWork project = new HomeWork();
+        project.oregon();
+        project.parade();
+        project.bbq();
+        project.hiking();
+        project.starfish();
+    }
+}
